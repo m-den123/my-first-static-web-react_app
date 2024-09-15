3ed410034e21700e4d2b193e561934a7933c4f28
m-den <m-den@sigmaint.co.jp>
2024-09-15 18:13:56 +0900

    feat: functionを追加

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
-    const value = '世界';
-    return <div>Hello {value}!!!</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const {text} = await( await fetch('/api/message_test1')).json();
+      setData(text);
+    })();
+  });
+  return <div>{data}</div>
+  //const value = '世界';
+  //return <div>Hello {value}!!!</div>;
 }
 
 export default App;
